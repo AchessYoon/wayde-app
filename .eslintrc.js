854a5da7d25a6098988f9db73ac2c805598e4432
Hyunsik Yoon <hsy6119@naver.com>
2023-02-09 20:00:00 +0900

    chore: add html-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "standard-with-typescript",
-        "airbnb",
-        "plugin:react/jsx-runtime",
-        "airbnb-typescript",
-        "airbnb/hooks",
-        "plugin:prettier/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "tsconfig.json"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "import/no-unresolved": "error"
-    },
-    "root": true,
+  "env": {
+      "browser": true,
+      "es2021": true
+  },
+  "extends": [
+      "plugin:react/recommended",
+      "standard-with-typescript",
+      "airbnb",
+      "plugin:react/jsx-runtime",
+      "airbnb-typescript",
+      "airbnb/hooks",
+      "plugin:prettier/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module",
+      "project": "tsconfig.json"
+  },
+  "plugins": [
+      "react"
+  ],
+  "rules": {
+      "import/no-unresolved": "error"
+  },
+  "root": true,
 }
