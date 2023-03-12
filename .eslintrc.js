baaa6042064b81de87e663d2e08d03225637f710
yoon_surface_wsl <hsy6119@naver.com>
2023-03-12 19:40:16 +0900

    chore: set eslint to handle html

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,37 @@
 module.exports = {
-  "env": {
-      "browser": true,
-      "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-      "plugin:react/recommended",
-      "standard-with-typescript",
-      "airbnb",
-      "plugin:react/jsx-runtime",
-      "airbnb-typescript",
-      "airbnb/hooks",
-      "plugin:prettier/recommended"
+  root: true,
+  overrides: [
+    {
+      files: ["*.+(js|jsx|ts|tsx)"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "tsconfig.json",
+      },
+      plugins: ["react"],
+      extends: [
+        "plugin:react/recommended",
+        "standard-with-typescript",
+        "airbnb",
+        "plugin:react/jsx-runtime",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:prettier/recommended",
+      ],
+      rules: {
+        "import/no-unresolved": "error",
+      },
+    },
+    {
+      files: "*.html",
+      parser: "@html-eslint/parser",
+      plugins: ["@html-eslint"],
+      extends: ["plugin:@html-eslint/recommended"],
+    },
   ],
-  "overrides": [
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module",
-      "project": "tsconfig.json"
-  },
-  "plugins": [
-      "react"
-  ],
-  "rules": {
-      "import/no-unresolved": "error"
-  },
-  "root": true,
-}
+};
